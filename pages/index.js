e95d09f48098bf97b697953919771a9aadd61dc3
Vanessa Motta <mottavns@gmail.com>
2022-01-30 22:19:37 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
-  
-  export default HomePage
+	return (
+		<div>
+			<h1>Boas vindas de volta!</h1>
+			<h2>Discord - Alura Matrix</h2>
+
+			<style jsx>{`
+        
+        h1 {
+          color: blue;
+        }
+      `}</style>
+		</div>
+	)
+}
+
+export default HomePage
